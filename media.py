826c4c8f016a1fe87d5c7ca9b04f37da37658056
lucas alves <santoslucas22498@gmail.com>
2025-09-04 09:17:28 -0300

    add cálculo de média

---
+++ media.py
@@ -0,0 +1,14 @@
+nota1 = float(input('digite sua 1ª nota  '))
+nota2 = float(input('digite sua 2ª nota  '))
+nota3 = float(input('digite sua 3ª nota  '))
+nota4 = float(input('digite sua 4ª nota  '))
+
+media = (nota1 + nota2 + nota3 + nota4)//4
+
+      
+if media >= 7.0:
+    print(f'média: {media} - situação :aprovado')
+elif media >= 5.0 < 7.0:
+    print(f'média: {media} - situação :recuperação')
+else:
+    print(f'média: {media} - situação :reprovado')
